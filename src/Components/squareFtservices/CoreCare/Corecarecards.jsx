44cb155b61292a736fbb35043e5a544483d544ff
Maham-Mehar <mahammehar47@gmail.com>
2025-03-03 02:30:58 -0800

    yyyy

--- src/Components/squareFtservices/CoreCare/Corecarecards.jsx
+++ src/Components/squareFtservices/CoreCare/Corecarecards.jsx
@@ -10,10 +10,11 @@ export const CoreCareFooter= () => {
             <Card className="p-1 main-Content shadow border-0 "style={{width:"223px" ,height:"100px"}}>
                 <Card.Body className="d-flex flex-column">
                     <div className="d-flex align-items-center gap-1">
-                        <MdAccessTime size={24} className="text-color" />
-                        <h6 className="text-color fw-normal mb-0">Cleaning Time</h6>
+                        <MdAccessTime size={24} style={{color:"rgba(4, 40, 98, 1)",}}/>
+                        <h6 className="fw-normal mb-0" style={{color:"rgba(4, 40, 98, 1)",}}>Cleaning Time</h6>
                     </div>
-                    <p className="secondText-color ps-4 fs-5 fw-normal mt-1">1-1.5 hours</p>
+                    <p className="ps-4 fs-5 fw-normal mt-1" style={{color:"rgba(59, 130, 246, 1)",
+}}>1-1.5 hours</p>
                 </Card.Body>
             </Card>
 
@@ -21,10 +22,11 @@ export const CoreCareFooter= () => {
             <Card className="p-1 shadow border-0"style={{width:"223px" ,height:"100px"}}>
                 <Card.Body className="d-flex flex-column">
                     <div className="d-flex align-items-center gap-1">
-                        <GoPeople size={24} className="text-color"/>
-                        <h6 className="text-color fw-normal mb-0">Number of People</h6>
+                        <GoPeople size={24} style={{color:"rgba(4, 40, 98, 1)",}}/>
+                        <h6 className="fw-normal mb-0" style={{color:"rgba(4, 40, 98, 1)",}}>Number of People</h6>
                     </div>
-                    <p className="secondText-color ps-4 fs-5 fw-medium mt-1">2-3 persons</p>
+                    <p className="ps-4 fs-5 fw-medium mt-1" style={{color:"rgba(59, 130, 246, 1)",
+}}>2-3 persons</p>
                 </Card.Body>
             </Card>
         </div>
